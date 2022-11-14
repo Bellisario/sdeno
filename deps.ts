a4ce74dc49a6a30d739141b4ae7294db6e7a1cc1
GitHub <noreply@github.com>
2022-11-14 17:34:45 +0000

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
-import * as fs from 'https://deno.land/std@0.163.0/node/fs/promises.ts';
-import * as os from 'https://deno.land/std@0.163.0/node/os.ts';
-import * as path from 'https://deno.land/std@0.163.0/path/mod.ts';
-import { Buffer } from 'https://deno.land/std@0.163.0/node/buffer.ts';
-import { mkdtemp } from 'https://deno.land/std@0.163.0/node/fs/promises.ts';
-import { strictEqual } from 'https://deno.land/std@0.163.0/node/assert/strict.ts';
+import * as fs from 'https://deno.land/std@0.164.0/node/fs/promises.ts';
+import * as os from 'https://deno.land/std@0.164.0/node/os.ts';
+import * as path from 'https://deno.land/std@0.164.0/path/mod.ts';
+import { Buffer } from 'https://deno.land/std@0.164.0/node/buffer.ts';
+import { mkdtemp } from 'https://deno.land/std@0.164.0/node/fs/promises.ts';
+import { strictEqual } from 'https://deno.land/std@0.164.0/node/assert/strict.ts';
 
 export { Buffer, fs, mkdtemp, os, path, strictEqual };
